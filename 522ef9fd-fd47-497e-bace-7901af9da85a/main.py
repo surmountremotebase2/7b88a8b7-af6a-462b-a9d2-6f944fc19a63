4bb7a90cbb4d5966b32bbb4ee56174a0ba3609ad
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-09-17 15:31:33 +0200

    User Nathan Smith saved changes to main.py at 09/17/2026, 13:31:32 UTC time.

--- 522ef9fd-fd47-497e-bace-7901af9da85a/main.py
+++ 522ef9fd-fd47-497e-bace-7901af9da85a/main.py
@@ -1,1 +1,14 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+
+
+class TradingStrategy(Strategy):
+    @property
+    def assets(self):
+        return ["SPY"]
+
+    @property
+    def interval(self):
+        return "1day"
+
+    def run(self, data):
+        return TargetAllocation({"SPY": 1.0})
